8171d335c6b6c44aa8f63ef716330924837fda57
Kasey Schlaudt <kaseyschlaudt@gmail.com>
2017-07-05 13:07:10 -0600

    Added print statement.

--- socialapp/ViewController.swift
+++ socialapp/ViewController.swift
@@ -21,6 +21,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("hi")
     }
 
     override func didReceiveMemoryWarning() {
